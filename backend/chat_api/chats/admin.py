f7e3c585b60b80347f80546a3be97bed033253ad
Kaan Özaltan <kaan.ozaltan@ug.bilkent.edu.tr>
2022-12-14 01:20:00 +0300

    Update message app admin

--- backend/chat_api/chats/admin.py
+++ backend/chat_api/chats/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Message
 
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'created_at',)
+
+
+admin.site.register(Message, MessageAdmin)
 
 
