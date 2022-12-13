99a7f008c042ac23e445064385f1414e67ebc8c2
Kaan Özaltan <kaan.ozaltan@ug.bilkent.edu.tr>
2022-12-13 13:14:18 +0300

    Create web socket

---
+++ chat-socket/index.js
@@ -0,0 +1,35 @@
+let express = require("express");
+let cors = require("cors");
+let http = require("http");
+let bodyParser = require("body-parser");
+let path = require("path");
+let socket = require("socket.io")();
+
+
+const port = 8000;
+
+app = express();
+
+const server = http.createServer(app).listen(port, () => { });
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "client")));
+
+app.use(bodyParser.json());
+
+app.post("/server", (req, res) => {
+    io.emit("command", req.body);
+    console.log(req.body);
+    res.status(201).json({ status: "reached" });
+});
+
+let io = socket.listen(server);
+
+io.on("connection", socket => {
+    socket.on("command", data => {
+        io.emit("command", data);
+    });
+});
+
+
